72cf2b5262f08b289791b9334bb25a104a76e4b3
Henrique A. Lourenço <91293974+Henrique-A-Lourenco@users.noreply.github.com>
2021-09-30 09:59:48 -0300

    Update ConnectionFactory.java

--- src/model/ConnectionFactory.java
+++ src/model/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class ConnectionFactory {
 
 	private static final String USERNAME = "root";
-	private static final String PASSWORD = "";//!@J4n3l4#$
+	private static final String PASSWORD = "";
 	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/cadastro";
 
 	public static Connection createConnectionToMySQL() throws Exception {
